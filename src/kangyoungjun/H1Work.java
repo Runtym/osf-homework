24b6c3164cb73502b75abe9c00b741cbd1f5c17f
appleater7 <appleater7@gmail.com>
2019-02-14 09:44:30 +0900

    .

--- src/kangyoungjun/H1Work.java
+++ src/kangyoungjun/H1Work.java
@@ -1,8 +1,8 @@
 package kangyoungjun;
 
 public class H1Work {
-
-	public static void main(String[] args) {
-		
-	}
+	// 기한은 2019-02-18 까지
+	// 캡쳐 1 : jdk 설치 후 eclipse 실행화면
+	// 캡쳐 2 : oracle 11 g express 설치 후
+	//          QueryBox 실행 및 접속 화면
 }
