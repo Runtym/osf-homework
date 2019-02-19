9bd16c7685bae356e448a4b8e0210c0e9cef179a
crisoberyl <crisoberyl@naver.com>
2019-02-19 23:37:38 +0900

    숙제 자료

--- src/lee/cho/rong/Overloading.java
+++ src/lee/cho/rong/Overloading.java
@@ -3,23 +3,26 @@ package lee.cho.rong;
 public class Overloading {
 
 	static int test() {
+		System.out.println("test() 오버로딩.");
 		return 0;
 	}
 	
 	static String test(String str) {
+		System.out.println("test(String str) 오버로딩.");
 		return "";
 	}
 	
 	static String test(int i) { 
+		System.out.println("test(int i) 오버로딩.");
 		return "";
 	}
 	
 	static void test(int i,String str1,String str2) {
-		
+		System.out.println("test(int i,String str1,String str2) 오버로딩.");
 	}
 	
 	static void test(String str1,String str2,int i) {
-		
+		System.out.println("test(String str1,String str2,int i) 오버로딩.");
 	}
 	
 	public static void main(String[] args) {
--- src/lee/cho/rong/Overriding.java
+++ src/lee/cho/rong/Overriding.java
@@ -2,17 +2,23 @@ package lee.cho.rong;
 
 public class Overriding {
 
-	void test2() {	}
+	void test2() {
+		System.out.println("오버라이딩 실패.");
+	}
 }
 
 class Overriding2 extends Overriding {
 
-	void test2() {	}
+	void test2() {
+		System.out.println("오버라이딩 실패.");
+	}
 }
 
 class Overriding3 extends Overriding2 {
 	
-	void test2() {	}
+	void test2() {
+		System.out.println("오버라이딩 성공.");
+	}
 
 	public static void main(String[] args) {
 		Overriding3 o3 = new Overriding3();
