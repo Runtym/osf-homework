dc06131bcbb09a52c50c1a27c1405340efad0168
appleater7 <appleater7@gmail.com>
2019-02-19 16:07:09 +0900

    .

--- src/kangyoungjun/H2Work.java
+++ src/kangyoungjun/H2Work.java
@@ -1,8 +1,33 @@
 package kangyoungjun;
 
+class Presentation0220 {
+	void Presentation0220(int jumSu1, int jumSu2, int jumSu3) {
+		System.out.println("셋중에 1등에게만 10,000원 상품권 준다!!");
+		// 비교후
+		System.out.println("1등은 누구");
+	}
+	
+	void Presentation0220(int jumSu1, int jumSu2, int jumSu3, int jumsu4) {
+		System.out.println("넷중에 1등에게만 10,000원 상품권 준다!!");
+		// 비교후
+		System.out.println("1등은 누구");
+	}
+	
+}
+
+class Man extends Presentation0220{
+	void Presentation0220(int jumSu1, int jumSu2, int jumSu3, int jumsu4) {
+		System.out.println("넷중에 1등에게만 10,000원 상품권 준다!!");
+		System.out.println("1등은");
+		// 비교후
+		System.out.println("1등은 누구");
+	}
+}
+
 public class H2Work {
 	// 기한은 2019-02-20 까지
 	// 오버로딩과 오버라이딩의 차이에 대해
 	// 앞에 나와서 발표할수 있도록
 	// 자료를 준비하여 올려주세요.
+
 }
