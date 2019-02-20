ef1e38a12b0bf74ff9b1f667cc592678c3e3fe19
junyoungkwon <junyoungkwon@hotmail.com>
2019-02-21 01:37:00 +0900

    setUp Java 1.8, eclipse, tomcat8.5, oracle11g made Overloading, Overriding examples

---
+++ src/kwonjunyoung/OverloadingTest.java
@@ -0,0 +1,23 @@
+package kwonjunyoung;
+
+class Dog2 {
+	public void bark() {
+		System.out.println("어흥!");
+	}
+
+	// Overloading Method
+	public void bark(int num) { // 같은 Method 이지만 다른 Parameter
+		for (int i = 0; i < num; i++) {
+			System.out.println("월월!");
+		}
+	}
+}
+
+public class OverloadingTest {
+	public static void main(String[] args) {
+		Dog2 d = new Dog2();
+		d.bark();
+		d.bark(3);
+
+	}
+}
