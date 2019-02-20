bf20fe7b30a63e0071f6ed3f82d27aa5bd4eeb3c
lgt8879 <lgt8879@gmail.com>
2019-02-20 19:19:15 +0900

    Overloading , OverRidng

---
+++ src/leekyutaek/Overloading.java
@@ -0,0 +1,24 @@
+package leekyutaek;
+
+public class Overloading {
+	public static int plus(int x, int y) {
+		System.out.println(x+y);
+		return 0;
+	}
+
+	public static int plus(int x, int y, int z, int q) {
+		System.out.println(x+y+z+q);
+		return 0;
+	}
+
+	public static String plus(String x, String y) {
+		System.out.println(x+y);
+		return null;
+	}
+
+	public static void main(String[] args) {
+		plus(1,2);
+		plus(4,1,5,6);
+		plus("12","34");
+	}
+}
