7e4b7ac2f0cedc7b00f9ac2cdec47fba038754e3
appleater7 <appleater7@gmail.com>
2019-02-21 09:50:06 +0900

    .

---
+++ src/kangyoungjun/Chicken.java
@@ -0,0 +1,14 @@
+package kangyoungjun;
+
+public class Chicken {
+
+	public String name;
+	
+	public void FriedChicken(Chicken c) {
+		this.name = "후라이드 치킨";
+	}
+	
+	public void Chicken(Chicken c, GalBiSauce s) {
+		this.name = "갈비치킨";
+	}
+}
