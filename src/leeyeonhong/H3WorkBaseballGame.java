c971d778c010addd69e40f958cd68feea069db7f
이연홍 <@SEC>
2019-02-26 00:05:11 +0900

    숙제 수정해서 제출

---
+++ src/leeyeonhong/H3WorkBaseballGame.java
@@ -0,0 +1,55 @@
+package leeyeonhong;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class H3WorkBaseballGame {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> AB = new ArrayList<>();
+		for (int i = 0; i < 4; i++) {
+			int ranNum = (int) (Math.random()*10);
+
+			if (AB.indexOf(ranNum) == -1) { 
+				AB.add(ranNum);
+			} else {
+				i--;
+			}
+		}
+
+
+		int ball = 0;
+		int strike = 0;
+		Scanner scan = new Scanner(System.in);
+		while (strike != 3) {
+			ball = 0;
+			strike = 0;
+			System.out.println("숫자 입력 하삼!");
+
+			String[] str = scan.nextLine().split(",");
+			int[] nums = new int[4];
+			for (int i = 0; i < str.length; i++) {
+				nums[i] = Integer.parseInt(str[i]);
+			}
+			for (int i = 0; i < nums.length; i++) {
+
+				if (AB.indexOf(nums[i]) != -1) {
+					if (i == AB.indexOf(nums[i])) {
+						strike++;
+					} else {
+						ball++;
+					}
+				}
+
+			}
+
+			if (strike + ball == 0) {
+				System.out.println("아웃!");
+			} else {
+				System.out.println(strike + "S, " + ball + "B");
+			}
+		}
+	}
+
+}
+
